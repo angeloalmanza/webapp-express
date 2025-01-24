86a020ce64608c6332da021466b233957740bf38
angeloalmanza <angeloalmanza26@gmail.com>
2025-01-24 18:04:21 +0100

    feat: implementazione cors

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const moviesRouter = require("./routers/movies");
 const errorsHandler = require("./middleware/errorsHandler");
 const notFound = require("./middleware/notFound");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.SERVER_PORT;
+
+//Middleware cors
+app.use(cors({
+    origin: process.env.REACT_URL
+}))
 
 //Middleware per la cartella public
 app.use(express.static('public'));
